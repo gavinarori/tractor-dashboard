fb5cf2eae8b7a139bd8166a81c863d9a0c15b478
gavinarori <arorigavin@gmail.com>
2024-11-20 20:09:33 +0300

    first commit

---
+++ src/layout/Sidebar.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from 'react'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import { getNavs } from '../navigation/index'
+import { logout } from '../store/Reducers/authReducer'
+import { BiLogInCircle } from 'react-icons/bi'
+import { useDispatch } from 'react-redux'
+import logo from '../assets/logo.png'
+
+const Sidebar = ({ showSidebar, setShowSidebar }) => {
+
+  const dispatch = useDispatch()
+  const navigate = useNavigate()
+
+  const { role } = useSelector(state => state.auth)
+  const { pathname } = useLocation()
+  const [allNav, setAllNav] = useState([])
+  useEffect(() => {
+    const navs = getNavs(role)
+    setAllNav(navs)
+  }, [role])
+
+  return (
+    <div>
+      <div onClick={() => setShowSidebar(false)} className={`fixed duration-200 ${!showSidebar ? 'invisible' : 'visible'} w-screen h-screen bg-[#22292f80] top-0 left-0 z-10`}></div>
+      <div className={`w-[260px] fixed bg-[#283046] z-50 top-0 h-screen shadow-[0_0_15px_0_rgb(34_41_47_/_5%)] transition-all ${showSidebar ? 'left-0' : '-left-[260px] lg:left-0'}`}>
+        <div className='h-[70px] flex justify-center items-center'>
+        <Link className='relative flex h-16 items-center justify-between gap-2 sm:gap-2' to='/'>
+              <img src="https://cdn.vectorstock.com/i/1000v/15/40/tractor-logo-emblem-design-vector-44971540.avif" className='h-16 w-16' alt="logo" />
+              <span className=' font-bold text-white text-[22px]'>Tractor Hub</span>
+        </Link>
+        </div>
+        <div className='px-[16px]'>
+          <ul>
+            {
+              allNav.map((n, i) => <li key={i}>
+                <Link to={n.path} className={`${pathname === n.path ? 'bg-slate-600 shadow-indigo-500/30 text-white duration-500 ' : 'text-[#d0d2d6] font-normal duration-200'} px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 `}>
+                  <span>{n.icon}</span>
+                  <span>{n.title}</span>
+                </Link>
+              </li>)
+            }
+            <li>
+              <button onClick={() => dispatch(logout({ navigate, role }))} className='text-[#d0d2d6] font-normal duration-200 px-[12px] py-[9px] rounded-sm flex justify-start items-center gap-[12px] hover:pl-4 transition-all w-full mb-1 '>
+                <span><BiLogInCircle /></span>
+                <span>Logout</span>
+              </button>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
