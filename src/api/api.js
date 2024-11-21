f57fdd5c3f68735928f1278031d01c74b9cfab47
gavinarori <arorigavin@gmail.com>
2024-11-21 09:12:08 +0300

    using local storage

--- src/api/api.js
+++ src/api/api.js
@@ -2,4 +2,14 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'https://tractor-backend-xk1t.onrender.com/api'
 })
-export default api
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+}, (error) => {
+    return Promise.reject(error);
+});
+
+export default api;
